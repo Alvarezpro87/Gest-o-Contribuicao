ac254f2f6f0375aeaf43525b5ac4827d8406f4a4
Alvarezpro87 <alvareznuvem@gmail.com>
2024-06-03 16:50:33 -0300

    subindo o backend

---
+++ Java/src/main/java/com/previdencia/gestaocontribuicao/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.previdencia.gestaocontribuicao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    public WebConfig() {
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(new String[]{"http://localhost:3000"}).allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"}).allowedHeaders(new String[]{"*"}).allowCredentials(true);
+            }
+        };
+    }
+}
