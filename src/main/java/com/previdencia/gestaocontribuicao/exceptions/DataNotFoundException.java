06d8d8aeb5e99061ae90fd900e16a88fed7ef50e
Alvarezpro87 <alvareznuvem@gmail.com>
2024-06-03 12:38:57 -0300

    Implementação de novos endpoints e ajustes no frontend

--- src/main/java/com/previdencia/gestaocontribuicao/exceptions/DataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.previdencia.gestaocontribuicao.exceptions;
-
-public class DataNotFoundException extends RuntimeException {
-    public DataNotFoundException(String message) {
-        super(message);
-    }
-}
